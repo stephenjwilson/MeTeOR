00cf9d7792536469a44b1f63068c54ffbeb3c2c8
wilson <sw5@bcm.edu>
2018-08-08 16:04:50 -0500

    switching back to naive pred through for loop

--- src/matlabpipeline/naivePrediction.m
+++ src/matlabpipeline/naivePrediction.m
@@ -3,7 +3,7 @@ function [CN,AA,Names1] = naivePrediction(NetName,AllNetworks)
 %   Detailed explanation goes here
 %% Get Data
 data=AllNetworks.(NetName);
-network=double(data{1}>0);
+network=data{1}>0;
 Names1=data{2};
 
 try
@@ -14,13 +14,28 @@ catch
     %% Transform the Network by equation
     % Find Common Neighbors
     disp('Setting up naive pred');
-    CN = network*network;
-    mask = eye(length(CN))>0;
-    degree = CN(mask);
-    CN(mask) = 0;
-    AA = diagsum(1./log(degree.*outer(network,network,0)),2,4);
-    AA(mask) = 0;
+    [i,j] = find(tril(ones(size(network)),-1));  % much faster than nchoosek
+    CN_values = zeros(length(i),1);
+    AA_values = zeros(length(i),1);
     
+    degree = sum(network,1);
+
+    disp('Calculating common neighbors');
+    % Calculate CN and AA
+    parfor ind=1:length(i)
+        if mod(ind,100000)==0
+            disp(ind);
+        end
+        common_neighbors = network(i(ind),:)& network(j(ind),:);
+        CN_values(ind) = sum(common_neighbors);
+        if CN_values(ind)>0
+            AA_values(ind) = sum(1 ./ log(degree(common_neighbors)));
+        end
+    end
+    % place into a network
+    [m,n] = size(network);
+    CN = sparse(i,j,CN_values,m,n);
+    AA = sparse(i,j,AA_values,m,n);
     save(sprintf('%s_naivepred.mat',NetName), 'CN', 'AA');
 end
 end
